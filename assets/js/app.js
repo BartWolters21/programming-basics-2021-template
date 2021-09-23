1b8dd8512e9c2fb5501a30b4cdd79c3a13fed9e3
Bart Wolters <bartw1999@gmail.com>
2021-09-23 10:13:39 +0200

    conditionals done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,48 @@
-console.log('JavaScript is working!')
-removeFromString();
+function askName() {
+  var person = prompt("Please enter your name.");
 
-function isItEven() {
-    const number = 2;
-    if (number % 2 === 0) {
-        console.log("The number is even.")
-    } else {
-        console.log("The number is not even.")
+  if (person == "Alice") {
+    document.getElementById("name").innerHTML =
+      "Hello " + person + "! How are you today?";
+  } else if (person == "Bob") {
+    document.getElementById("name").innerHTML =
+      "Hello " + person + "! How are you today?";
+  } else {
+    document.getElementById("name").innerHTML =
+      person + " Wil je zakje chips oeleh?";
+  }
+}
+
+function bookLog() {
+  purchasedBook = true;
+  job = "teacher";
+  inTrain = false;
+
+  if (purchasedBook && job === "teacher" && !inTrain) {
+    console.log("Finally I can enjoy my book!");
+  } else {
+    console.log("You can't read this.");
+  }
+}
+
+function gradeStudent(){
+    grade = 8;
+
+    if(grade < 6){
+        console.log("Insufficient");
+    } else if(grade > 6 && grade < 7){
+        console.log("Sufficient");
+    } else if(grade > 7 && grade < 9) {
+        console.log("Good");
+    } else if(grade > 9){
+        console.log("Excellent");
     }
 }
 
-function removeFromString() {
-    const string = "Programming is not so cool";
-    const removed = string.replace("not ", "")
-    console.log(removed)
+function inputDate(){
+    const year = prompt("Please input a year.");
+    const month = prompt("Please input the number of a month");
+    const date = new Date(year, month, 0).getDate();
+
+    console.log(date);
 }
