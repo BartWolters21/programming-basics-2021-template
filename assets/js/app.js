c7a99e9cc34146a71745ec652405b0f6e0a96b66
Bart Wolters <bartw1999@gmail.com>
2021-09-30 10:37:36 +0200

    added arrays

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,57 +1,38 @@
-function drawDiamond() {
-    var canvas = getElement();
-    if (canvas.getContext){
-      //Ruby's
-      var x=100; var y=100;
-      var w=200; var h=200;
-      var ctx = canvas.getContext('2d');
-      color=('#FF0000');
-      sketch(x,y,w,h,ctx,color);
-  
-    }
-  }
+const lapRounds = [2.99, 3.00, 3.01, 4.01, 2.79, 2.88, 3.10, 4.12];
+// get random number from array
+function randomElement(){
+  randomItem = lapRounds[Math.floor(Math.random()*lapRounds.length)];
 
-  function getElement() {
-    return document.getElementById('myCanvas')
+  console.log(randomItem);
 }
-  
-   function sketch(x,y,w,h,ctx,color){
-      ctx.fillStyle=color;
-      ctx.beginPath();
-      ctx.moveTo(x,y);
-      ctx.lineTo(x+w/2,y+0.7*h);
-      ctx.lineTo(x+w/2,y);
-      ctx.fill();
-      
-      ctx.fillStyle=color;
-      ctx.beginPath();
-      ctx.moveTo(x+w/2, y);
-      ctx.lineTo(x+w/2,y+0.7*h);
-      ctx.lineTo(x+w,y);
-      ctx.fill();
-      
-      // Upper left triangle
-      ctx.beginPath();
-      ctx.moveTo(x+w/4,y-0.3*h);
-      ctx.lineTo(x,y);
-      ctx.lineTo(x+w/2,y);
-      ctx.fill();
-      
-      // centre inverted triangle
-      ctx.fillStyle=color;
-      ctx.beginPath();
-      ctx.moveTo(x+w/4,y-0.3*h);
-      ctx.lineTo(x+w/2,y);
-      ctx.lineTo(x+0.75*w,y-0.3*h);
-      ctx.fill();
-      
-      //Upper left triangle.
-      ctx.fillStyle=color;
-      ctx.beginPath();
-      ctx.moveTo(x+0.75*w,y-0.3*h);
-      ctx.lineTo(x+w/2,y);
-      ctx.lineTo(x+w,y);
-      ctx.fill();
-    
-      
-   }
+
+//show all records in console
+function allRecords(){
+  const allMyRecords = [
+    [
+      "The Who - Pinball Wizard", 
+      "Rolling Stones - Exile on main street", 
+      "Police - Message in a bottle"
+    ],
+    [
+      "De Dijk - Alle 40 Goed", 
+      "Het Goede Doel - Belgie", 
+      "Doe Maar - skunk"
+    ]
+  ];
+
+  for (let i = 0; i < allMyRecords.length; i++) {
+    for (let k = 0; k < allMyRecords[i].length; k++) {
+        console.log(allMyRecords[i][k])
+    }
+}
+}
+s
+// Filter all numbers from array that are smaller than 4
+function filteredLapRoundsWithForLoop() {
+    const lapRounds = [2.99, 3.00, 3.01, 4.01, 2.79, 2.88, 3.10, 4.12];
+    const lessThanFour = lapRounds.filter(function (number) {
+        return number < 4;
+    });
+    console.log(lessThanFour);
+}
